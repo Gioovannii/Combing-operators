473854f06e766d703d05ea7942f6f08766eb6d6a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-14 22:15:46 +0800

    example prepend publisher

--- Starter.playground/Contents.swift
+++ Starter.playground/Contents.swift
@@ -42,6 +42,22 @@ example(of: "prepend(Publisher)") {
     
 }
 
+example(of: "prepend(Publisher) #2") {
+    // 1 Create two publisher
+    let publisher1 = [3, 4].publisher
+    let publisher2 = PassthroughSubject<Int, Never>()
+    
+    // 2 Prepend subject before publisher1
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    // 3 send value to publisher 2
+    publisher2.send(1)
+    publisher2.send(2)
+    publisher2.send(completion: .finished)
+}
 
 
 
