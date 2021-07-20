45f667e7c3ca0fd0e884b6dc0ef65857b1710588
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-20 17:20:10 +0200

    set combine latest

--- Starter.playground/Contents.swift
+++ Starter.playground/Contents.swift
@@ -219,7 +219,13 @@ example(of: "combineLatest") {
     let publisher1 = PassthroughSubject<Int, Never>()
     let publisher2 = PassthroughSubject<String, Never>()
     
-   
+    // 2
+    publisher1
+        .combineLatest(publisher2)
+        .sink(receiveCompletion: { _ in print("Completed") },
+              receiveValue: { print("P1: \($0); P2: \($1)") })
+        .store(in: &subscriptions)
+  
 }
 
 // Copyright (c) 2020 Razeware LLC
